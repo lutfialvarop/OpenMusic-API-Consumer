d0310509da0ce0f02328ec8bd216fab8385c615f
Lutfi Alvaro Pratama <78428836+lutfialvarop@users.noreply.github.com>
2025-09-07 23:34:30 +0700

    feat: update ESLint configuration and add start script; refactor service classes for consistency

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,6 @@ import daStyle from 'eslint-config-dicodingacademy';
 
 export default defineConfig([
   daStyle,
-  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: { ...globals.browser, ...globals.node } }, rules: { 'linebreak-style': ['error', 'windows'] } },
   { files: ['**/*.js'], languageOptions: { sourceType: 'commonjs' } },
 ]);
